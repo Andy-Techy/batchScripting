b865c6ef7f3f58049798676f61ff5d5ac27d0bb3
Michael Andrew Adjei <adjeimitchel03@gmail.com>
2025-02-07 10:18:49 +0000

    recieving user input

---
+++ tutorials/input.bat
@@ -0,0 +1,14 @@
+@REM creating a dynamic content 
+
+@REM SET /P allows to take user input
+
+@REM sytax === SET /P variable_name=Your prompt message
+
+@echo off
+SET /P name = tell me your name: 
+
+echo ******************************
+echo alright
+echo ******************************
+
+pause
